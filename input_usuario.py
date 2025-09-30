13f9674571910f6af1416a5318936f3de8e36076
Marcelo Valdez <marcelogvz27@gmail.com>
2025-09-29 23:33:13 -0300

    un cambio bastente bueno jeje

--- input_usuario.py
+++ input_usuario.py
@@ -6,8 +6,8 @@ class UsuarioEntrada:
     def pedir_coordenadas(mensaje, mapa):
         while True:
             try:
-                fila = int(input(F"ingrese fila de {mensaje}(1-{mapa.filas}): ")) 
-                columna = int(input(F"ingrese columna de {mensaje}(1-{mapa.columnas}): "))
+                fila = int(input(F"ingrese fila de {mensaje}(1-{mapa.filas}): ")) - 1 
+                columna = int(input(F"ingrese columna de {mensaje}(1-{mapa.columnas}): ")) - 1
             except ValueError:
                 print("Ingresaste algo que no es un numero entero, intenta de nuevo!")
                 continue
